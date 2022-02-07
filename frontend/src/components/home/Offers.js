6938da5894813c53bef4630223ec7942f9fa0522
fromabby <60252891+fromabby@users.noreply.github.com>
2022-02-07 16:46:33 +0800

    migrating to react bootstrap

--- frontend/src/components/home/Offers.js
+++ frontend/src/components/home/Offers.js
@@ -4,29 +4,29 @@ import { Container, Row, Col } from 'react-bootstrap'
 const Offers = () => {
     return (
         <div className="idea-section">
-            <div className='some-page-wrapper'>
-                <div className='row'>
-                    <div className='column'>
-                        <div className='blue-column'>
-                            <h1>Charity of Choice</h1>
-                            <p>The team will pick a charity of its choosing to award a portion of the sales. The suggestions of the community will be greatly considered, especially the vote of the Haute Gang.</p>
-                            <p>The allocated funds will be used every four cycles to be donated to the charity of choice. For full transparency, it will be documented in the NGC Discord Server.</p>
-                        </div>
-                    </div>
-                    <div className='column'>
-                        <div className='green-column'>
-                            <h1>Community Fund</h1>
-                            <p>NEAR Gang Couture thrives in a community of artists. It aims to help struggling and upstart artists in the space by giving them a “Blue Dot Cutie” moment where the fund personally purchases art from them, and helps promote it using its platform.</p>
-                            <p>This will be managed by a fund manager who will also be responsible for awarding new artists .5N+ for them to start their wallet and be able to mint their art pieces as well.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <h1>Charity of Choice</h1>
+                        <p>The team will pick a charity of its choosing to award a portion of the sales. The suggestions of the community will be greatly considered, especially the vote of the Haute Gang.</p>
+                        <p>The allocated funds will be used every four cycles to be donated to the charity of choice. For full transparency, it will be documented in the NGC Discord Server.</p>
+                    </Col>
+                    <Col md={6}>
+                        <h1>Community Fund</h1>
+                        <p>NEAR Gang Couture thrives in a community of artists. It aims to help struggling and upstart artists in the space by giving them a “Blue Dot Cutie” moment where the fund personally purchases art from them, and helps promote it using its platform.</p>
+                        <p>This will be managed by a fund manager who will also be responsible for awarding new artists .5N+ for them to start their wallet and be able to mint their art pieces as well.</p>
+                    </Col>
+                </Row>
+            </Container>
+
+
         </div>
     )
 }
 
 export default Offers
+
+
 
 // Charity of Choice:
 // The team will pick a charity of its choosing to award a portion of the sales. The suggestions of the community will be greatly considered, especially the vote of the Haute Gang.
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,15 +1,40 @@
 import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import './Header.css'
+
 
 const Header = () => {
     return (
-        <div>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="header">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <div className="logo">
+                            <a href="#"><span style={{ color: "#FF9601" }}>NEAR GANG</span><span style={{ paddingLeft: "5px", color: "#fff" }}> COUTURE</span></a>
+                        </div>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#idea-section">what we do</Nav.Link>
+                            <Nav.Link href="#team-section">who we are</Nav.Link>
+                            <Nav.Link href="#gallery">gallery</Nav.Link>
+                            <Nav.Link href="https://paras.id/neargangcouture.near/collections" target="_blank">paras collection</Nav.Link>
+                            {/* <Nav.Link eventKey={2} href="#memes">Dank memes</Nav.Link> */}
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
+        {/* <div>
             <header className="header">
                 <div className="container">
                     <div className="row align-items-center justify-content-between">
-                        <div className="logo">
-                            <a href="#"><span style={{color: "#FF9601"}}>NEAR GANG</span><span style={{paddingLeft: "5px",color: "#fff"}}> COUTURE</span></a>
-                        </div>
+                        
                         <input type="checkbox" id="nav-check"/>
                         <label for="nav-check" className="nav-toggler">
                             <span></span>
@@ -25,8 +50,10 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-        </div>
+        </div> */}
+        </>
     )
 }
 
 export default Header
+
