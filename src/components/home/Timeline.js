a5f5246b7c0c8cda0f87e1f9daa390984298c648
fromabby <60252891+fromabby@users.noreply.github.com>
2022-03-11 18:28:32 +0800

    Update Timeline.js

--- src/components/home/Timeline.js
+++ src/components/home/Timeline.js
@@ -70,31 +70,3 @@ const Timeline = () => {
 }
 
 export default Timeline
-
-// Short-term Timeline:
-
-// January 2022
-// Inception of Idea
-// Creation of Base Assets
-// Securing Core Team
-
-// February 2022
-// Discord Launch
-// Twitter Launch
-// Creation of Peripheral Assets
-// Securing Partners
-// White Paper Reveal
-// Art Reveal
-// Giveaways and Promotions
-// Whitelisting Process
-
-// March 2022
-// Whitelist Genesis Auction
-// Genesis Drop
-// Cycle 1 of Merging Start
-
-// April 2022
-// Big Asset Giveaway
-// Community Auction
-// Charity Drive Launch
-// 2 Cycle Pause on Merging
